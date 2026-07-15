f414654d736e2732d120ade51b7672c61357f7f3
shir.cohen <shir.cohen83@gmail.com>
2026-07-15 18:45:59 +0300

    making app and main connected to react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,22 @@
 import "./styles/design-system.css";
-import "./App.css";
 
-import logo from "./assets/images/logo.png";
-import InfoCard from "./components/InfoCard";
-import WeeklySchedule from "./components/WeeklySchedule";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import LessonsPage from "./pages/LessonsPage";
 
-const lessons = [
-    {
-        id: "zumba",
-        title: "זומבה",
-        day: 0,
-        startHour: 17,
-        duration: 1,
-    },
-    {
-        id: "kickboxing",
-        title: "קיקבוקס",
-        day: 2,
-        startHour: 18,
-        duration: 1,
-    },
-    {
-        id: "general-sport",
-        title: "ספורט כללי",
-        day: 4,
-        startHour: 16,
-        duration: 1,
-    },
-    {
-      id: "general-sport",
-      title: "ספורט כללי",
-      day: 0,
-      startHour: 10,
-      duration: 1,
-    },
-];
 
 function App() {
   return (
-    <main className="home-page">
-      <img src={logo} alt="SHtodio Logo" className="logo" />
-      <div className="info-cards-container">
-        
-        <InfoCard
-          title="מי אני?"
-          description="אני רוצה להנגיש ספורט לילדים כי גם אני גדלתי עם התחושה שספורט מחובר לציונים, ביצועים ולחץ."
-        />
-
-        <InfoCard
-          title="מי אנחנו?"
-          description="SHtodio הוא סטודיו כושר לילדים שמטרתו להפוך ספורט לחוויה כיפית, משוחררת ולא תחרותית."
-        />
-
-        <InfoCard
-          title="איך מתחילים?"
-          description="לוחצים על שיעור במערכת השעות, יוצרים חשבון או נכנסים לחשבון קיים ונרשמים לשיעור."
-        />
-
-      </div>
-
-   =<WeeklySchedule lessons={lessons} />
-      
-    </main>
+   <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/lessons/zumba"
+        element={<h1 style={{ color: "black" }}>זומבה עובדת</h1>}
+      />
+      <Route path="/lessons/:lessonType" element={<LessonsPage />} />
+      {/* <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} /> */}
+  </Routes>
   );
 }
 
